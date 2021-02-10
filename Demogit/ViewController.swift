fae518bb619aa10beac1fcd991dc888e102416e6
Amrita <amrita@Sushmas-MacBook-Pro.local>
2021-02-10 20:34:40 +0530

    git intial proj demo

---
+++ Demogit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Demogit
+//
+//  Created by Amrita on 10/02/21.
+//  Copyright © 2021 Amrita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
